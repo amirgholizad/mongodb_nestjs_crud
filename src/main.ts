6508b1fdfa97f3515dd4c91bf19833fffc6bd197
amirg <agholizadgha@mun.ca>
2025-03-23 16:23:21 -0230

    init remote db connection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const post = await prisma.user.create({
+    data: {
+      title: 'Hello world222',
+      content: 'This is a test post222',
+    },
+  });
+  console.log(post);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
